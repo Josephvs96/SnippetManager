92b7fe2f009d3164efefbd24bc167efea487014f
Joseph bahhadi <josephbhd95@outlook.com>
2021-03-14 21:19:25 +0100

    Fix the Async await problem with some of the functions

--- SnippetManager/Startup.cs
+++ SnippetManager/Startup.cs
@@ -54,6 +54,7 @@ namespace SnippetManager
             services.AddMudServices();
             services.AddTransient<ClipboardService>();
             services.AddScoped<StatisticsService>();
+            services.AddApplicationInsightsTelemetry();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
